bc0eac7e682673945ee22361dbe30227279fd97c
CezinhaDev <cesarhenriquee04@gmail.com>
2025-07-08 20:04:56 -0300

    feito

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+        System.out.println("teste ");
+        for (int i = 0; i <10 ; i++) {
+            System.out.println("teste teste");
+        }
+        }
+    }
